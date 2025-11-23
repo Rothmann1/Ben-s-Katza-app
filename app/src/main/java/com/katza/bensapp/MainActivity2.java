bee4d9ee7764c61c5c5fc6aa2b39dd77c45250ce
Eitan <eitankatzir231008@gmail.com>
2025-11-23 16:43:19 +0200

    Dynamic

---
+++ app/src/main/java/com/katza/bensapp/MainActivity2.java
@@ -0,0 +1,37 @@
+package com.katza.bensapp;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    ImageView iv;
+    ConstraintLayout main;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        iv = new ImageView(this);
+        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(1000,1000);
+        iv.setLayoutParams(layoutParams);
+
+        main = (ConstraintLayout)findViewById(R.id.main);
+        iv.setImageResource(R.drawable.download);
+        main.addView(iv);
+    }
+}
